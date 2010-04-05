07821d081aa3f54140aa5d8651ecb24eacc70658
Dean Clatworthy <dean.clatworthy@gmail.com>
2010-04-05 14:33:38 +0300

    default notifications config

--- app/config/notifications.default.php
+++ app/config/notifications.default.php
@@ -1,1 +1,14 @@
 <?php
+
+$config['Notifications'] = array(
+	'smtp' => array(
+		'port' => '25'
+		,'timeout' => '30'
+		,'host' => ''
+		,'username' => ''
+		,'password' => ''
+		,client' => 'smtp_helo_hostname'
+	)
+);
+
+?>
