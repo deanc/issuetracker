8f4b9a2ef9dd3462f1505fec543ff505b4f435ab
Dean <dean.clatworty@gmail.com>
2010-11-30 21:35:42 +0200

    ffs

--- app/controllers/projects_controller.php
+++ app/controllers/projects_controller.php
@@ -2,7 +2,7 @@
 
 class ProjectsController extends AppController
 {
-    var $uses = array('Project', 'Issue', 'User');
+    var $uses = array('Project', 'Issue', 'User', 'IssuePriority', 'ProjectUser');
 	var $helpers = array('Javascript', 'Paginator', 'Time', 'Text');
 	var $components = array('RequestHandler');
 
@@ -28,13 +28,23 @@ class ProjectsController extends AppController
             ");
     //			Cache::write('statuses', $statuses);
     //		}
+		$s = array();
+		foreach($statuses AS $status)
+		{
+			$s[$status['IssueStatus']['status_id']] = $status['IssueStatus']['status'];// . ' (' . $status['sq']['total'] . ')';
+		}
+
             $this->set('_statuses', $statuses);
-        }
+		$this->set('_statuses2', $s);
+        
+		$this->set('priorities', $this->Issue->IssuePriority->find('list', array('fields' => array('IssuePriority.priority'))));
+	}
     }
 
 	function index()
 	{
-        $userinfo = $this->Session->read('userinfo');
+        
+		$userinfo = $this->Session->read('userinfo');
         $conditions = array('Project.public' => 1);
         if(!empty($userinfo))
         {
@@ -45,7 +55,7 @@ class ProjectsController extends AppController
             'hasOne' => array(
                 'ProjectUser'
         )));
-       $this->Project->recursive = 0;
+        $this->Project->recursive = 0;
 
         $projects = $this->Project->find('all', array(
                 'fields' => array('Project.*'),
@@ -69,31 +79,87 @@ class ProjectsController extends AppController
 		$this->set('projects', $projects);
 	}
 
-    function issues($project_id, $status = 0)
+    function issues($project_id, $status = 0, $priority = 0)
     {
         $this->set('project', $this->Project->findByproject_id($project_id));
 
  		$this->paginate = array(
-			'contain' => array('User', 'IssueStatus')
+			'contain' => array('User', 'IssueStatus', 'IssuePriority')
 			, 'limit' => 20
 			,'order' => 'Issue.updated DESC'
 		);
 
 		// look for url params
-		$status_id = $this->IssueStatus->findBystatus($status);
+	$conditions = array('Issue.project_id' => (int)$project_id);
+	if(isset($_GET['status_id']))
+	{	
+		$status_id = $this->IssueStatus->findBystatus_id(intval($_GET['status_id']));
 
 		if($status_id > 0)
 		{
 			$this->set('status', $status_id['IssueStatus']['status']);
 			$status_id = $status_id['IssueStatus']['status_id'];
 			$this->paginate = array_merge($this->paginate, array('conditions' => array('Issue.status_id' => $status_id, 'Issue.project_id' => (int)$project_id)));
+			$conditions['Issue.status_id'] = $status_id;
 		}
+
+		if(isset($_GET['priority_id']))
+		{
+			$priority = $this->IssuePriority->findBypriority_id(intval($_GET['priority_id']));
+
+			if(!empty($priority))
+			{
+				$conditions['Issue.priority_id'] = $priority['IssuePriority']['priority_id'];
+			}
+		}
+	}
+
+	$this->paginate = array_merge($this->paginate, array('conditions' => $conditions));
+
 
     	$issues = $this->paginate('Issue');
 
     	$this->set(compact('issues'));
     }
 
+    function users($project_id, $issue_id = 0)
+	{
+		$this->ProjectUser->primaryKey = 'user_id';
+		$this->ProjectUser->bindModel(array(
+			'hasOne' => array('User' => array('className' => 'User', 'foreignKey' => 'user_id'))
+		));
+
+		$conditions = array('ProjectUser.project_id' => $project_id);
+		if(isset($_GET['tag']) AND !empty($_GET['tag']))
+		{
+			$conditions[] = 'User.username LIKE \'%' . preg_replace('/[^A-Z0-9]+/i', '', $_GET['tag']) . '%\'';
+		}
+		else
+		{
+			echo json_encode(array());
+			die;
+		}		
+
+		$users = $this->ProjectUser->find('all', array('conditions' => $conditions));
+
+	//	$users = $this->ProjectUser->find('all', array('conditions' => array('ProjectUser.project_id' => $project_id), 'contain' => array('User')));
+		
+		$existing = explode(',', $_GET['id_list']);
+		$existing = array_unique($existing);
+
+		$json = array();
+		foreach($users as $u)
+		{
+			if(!in_array($u['ProjectUser']['user_id'], $existing)) {
+				$json[] = array(
+					'value' => $u['ProjectUser']['user_id']
+					,'caption' => $u['User']['username']
+				);
+			}
+		}
+		echo json_encode($json);
+		die;
+	}
 }
 
 ?>
